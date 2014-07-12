d3561c921d38a3db9d57f11c7ddc97e1c07644fe
Akihiro Maeda <sola.1980.a@gmail.com>
2014-07-12 22:04:06 +0900

    Replaced to TWRP-based recovery

--- device.mk
+++ device.mk
@@ -31,8 +31,12 @@ PRODUCT_COPY_FILES := \
     $(LOCAL_KERNEL):kernel
 
 PRODUCT_COPY_FILES += \
+    device/lge/hammerhead/twrp.fstab:recovery/root/etc/twrp.fstab
+
+PRODUCT_COPY_FILES += \
     device/lge/hammerhead/init.hammerhead.rc:root/init.hammerhead.rc \
     device/lge/hammerhead/init.hammerhead.usb.rc:root/init.hammerhead.usb.rc \
+    device/lge/hammerhead/init.recovery.hammerhead.rc:root/init.recovery.hammerhead.rc \
     device/lge/hammerhead/fstab.hammerhead:root/fstab.hammerhead \
     device/lge/hammerhead/ueventd.hammerhead.rc:root/ueventd.hammerhead.rc
 
