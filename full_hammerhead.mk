86874aa22a878b09741d7a6656c95df4fab481c0
Akihiro Maeda <sola.1980.a@gmail.com>
2014-11-12 00:56:19 +0900

    Add makefiles of device/aosp and vendor/aosp

--- full_hammerhead.mk
+++ full_hammerhead.mk
@@ -32,3 +32,5 @@ PRODUCT_RESTRICT_VENDOR_FILES := true
 
 $(call inherit-product, device/lge/hammerhead/device.mk)
 $(call inherit-product-if-exists, vendor/lge/hammerhead/device-vendor.mk)
+$(call inherit-product-if-exists, vendor/aosp/lge/hammerhead/full.mk)
+$(call inherit-product, device/aosp/common/full.mk)
